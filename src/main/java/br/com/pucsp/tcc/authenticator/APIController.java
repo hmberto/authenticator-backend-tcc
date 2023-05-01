ecc51eab60cdfb25478b36b8964e5640b2491e80
Humberto <humbertoodantas@gmail.com>
2023-04-30 21:01:37 -0300

    Jetty server config

--- src/main/java/br/com/pucsp/tcc/authenticator/APIController.java
+++ src/main/java/br/com/pucsp/tcc/authenticator/APIController.java
@@ -1,77 +1,36 @@
 package br.com.pucsp.tcc.authenticator;
 
-import br.com.pucsp.tcc.authenticator.rest.CheckAccessLinkService;
-import br.com.pucsp.tcc.authenticator.rest.CheckSessionService;
-import br.com.pucsp.tcc.authenticator.rest.GetUserDataService;
-import br.com.pucsp.tcc.authenticator.rest.LogoutService;
-import br.com.pucsp.tcc.authenticator.rest.RegisterEmailService;
-import br.com.pucsp.tcc.authenticator.rest.RegisterNameService;
-import br.com.pucsp.tcc.authenticator.rest.TestService;
-import br.com.pucsp.tcc.authenticator.rest.ValidateAccessLinkService;
-import br.com.pucsp.tcc.authenticator.rest.ValidateOtpService;
-import br.com.pucsp.tcc.authenticator.rest.error.MethodNotAllowedFilter;
-import br.com.pucsp.tcc.authenticator.rest.error.NotFoundServlet;
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
 
-import java.util.EnumSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.servlet.DispatcherType;
+public class APIController extends HttpServlet {
+	private static final Logger LOGGER = LoggerFactory.getLogger(APIController.class);
+	private static final long serialVersionUID = 1L;
 
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.servlet.FilterHolder;
-
-public class APIController {
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
+		JettyService.start();
+	}
 
-		FilterHolder methodNotAllowedFilterHolder = new FilterHolder(new MethodNotAllowedFilter());
-		ServletContextHandler rootContext = new ServletContextHandler();
-		ServletContextHandler apiContext = new ServletContextHandler();
-		
-		rootContext.setContextPath("/");
-		apiContext.setContextPath("/api");
-		
-		rootContext.addServlet(new ServletHolder(new NotFoundServlet(rootContext)), "/*");
-		apiContext.addServlet(new ServletHolder(new NotFoundServlet(apiContext)), "/*");
-		
-		rootContext.addServlet(new ServletHolder(new TestService()), "/test");
-		rootContext.addFilter(methodNotAllowedFilterHolder, "/test", EnumSet.of(DispatcherType.REQUEST));
-		
-		apiContext.addServlet(new ServletHolder(new TestService()), "/test");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/test", EnumSet.of(DispatcherType.REQUEST));
-		
-		apiContext.addServlet(new ServletHolder(new RegisterEmailService()), "/register/email");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/register/email", EnumSet.of(DispatcherType.REQUEST));
+	public void init(ServletConfig config) throws ServletException {
+		try {
+			JettyService.start();
+		} catch (Exception e) {
+			LOGGER.error("Error starting jetty", e);
+		}
+	}
 
-		apiContext.addServlet(new ServletHolder(new RegisterNameService()), "/register/name");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/register/name", EnumSet.of(DispatcherType.REQUEST));
-		
-		apiContext.addServlet(new ServletHolder(new ValidateOtpService()), "/validate/otp");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/validate/otp", EnumSet.of(DispatcherType.REQUEST));
-		
-		apiContext.addServlet(new ServletHolder(new ValidateAccessLinkService()), "/validate/access-link");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/validate/access-link", EnumSet.of(DispatcherType.REQUEST));
-		
-		apiContext.addServlet(new ServletHolder(new CheckAccessLinkService()), "/check/access-link/*");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/check/access-link/*", EnumSet.of(DispatcherType.REQUEST));
-		
-		apiContext.addServlet(new ServletHolder(new CheckSessionService()), "/check/session");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/check/session", EnumSet.of(DispatcherType.REQUEST));
-		
-		apiContext.addServlet(new ServletHolder(new GetUserDataService()), "/users/*");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/users/*", EnumSet.of(DispatcherType.REQUEST));
-		
-		apiContext.addServlet(new ServletHolder(new LogoutService()), "/logout");
-		apiContext.addFilter(methodNotAllowedFilterHolder, "/logout", EnumSet.of(DispatcherType.REQUEST));
-		
-		ContextHandlerCollection contexts = new ContextHandlerCollection();
-		contexts.setHandlers(new Handler[] { apiContext, rootContext });
-		server.setHandler(contexts);
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		LOGGER.info("APIController called");
+	}
 
-		server.start();
-		server.join();
+	public void destroy() {
+		super.destroy();
 	}
 }
---
+++ src/main/java/br/com/pucsp/tcc/authenticator/JettyService.java
@@ -0,0 +1,77 @@
+package br.com.pucsp.tcc.authenticator;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import br.com.pucsp.tcc.authenticator.rest.CheckAccessLinkService;
+import br.com.pucsp.tcc.authenticator.rest.CheckSessionService;
+import br.com.pucsp.tcc.authenticator.rest.GetUserDataService;
+import br.com.pucsp.tcc.authenticator.rest.LogoutService;
+import br.com.pucsp.tcc.authenticator.rest.RegisterEmailService;
+import br.com.pucsp.tcc.authenticator.rest.RegisterNameService;
+import br.com.pucsp.tcc.authenticator.rest.TestService;
+import br.com.pucsp.tcc.authenticator.rest.ValidateAccessLinkService;
+import br.com.pucsp.tcc.authenticator.rest.ValidateOtpService;
+import br.com.pucsp.tcc.authenticator.rest.error.MethodNotAllowedFilter;
+import br.com.pucsp.tcc.authenticator.rest.error.NotFoundServlet;
+
+public class JettyService {
+	public static void start() throws Exception {
+		Server server = new Server(8080);
+
+		FilterHolder methodNotAllowedFilterHolder = new FilterHolder(new MethodNotAllowedFilter());
+		ServletContextHandler rootContext = new ServletContextHandler();
+		ServletContextHandler apiContext = new ServletContextHandler();
+		
+		rootContext.setContextPath("/");
+		apiContext.setContextPath("/api");
+		
+		rootContext.addServlet(new ServletHolder(new NotFoundServlet(rootContext)), "/*");
+		apiContext.addServlet(new ServletHolder(new NotFoundServlet(apiContext)), "/*");
+		
+		rootContext.addServlet(new ServletHolder(new TestService()), "/test");
+		rootContext.addFilter(methodNotAllowedFilterHolder, "/test", EnumSet.of(DispatcherType.REQUEST));
+		
+		apiContext.addServlet(new ServletHolder(new TestService()), "/test");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/test", EnumSet.of(DispatcherType.REQUEST));
+		
+		apiContext.addServlet(new ServletHolder(new RegisterEmailService()), "/register/email");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/register/email", EnumSet.of(DispatcherType.REQUEST));
+
+		apiContext.addServlet(new ServletHolder(new RegisterNameService()), "/register/name");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/register/name", EnumSet.of(DispatcherType.REQUEST));
+		
+		apiContext.addServlet(new ServletHolder(new ValidateOtpService()), "/validate/otp");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/validate/otp", EnumSet.of(DispatcherType.REQUEST));
+		
+		apiContext.addServlet(new ServletHolder(new ValidateAccessLinkService()), "/validate/access-link");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/validate/access-link", EnumSet.of(DispatcherType.REQUEST));
+		
+		apiContext.addServlet(new ServletHolder(new CheckAccessLinkService()), "/check/access-link/*");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/check/access-link/*", EnumSet.of(DispatcherType.REQUEST));
+		
+		apiContext.addServlet(new ServletHolder(new CheckSessionService()), "/check/session");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/check/session", EnumSet.of(DispatcherType.REQUEST));
+		
+		apiContext.addServlet(new ServletHolder(new GetUserDataService()), "/users/*");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/users/*", EnumSet.of(DispatcherType.REQUEST));
+		
+		apiContext.addServlet(new ServletHolder(new LogoutService()), "/logout");
+		apiContext.addFilter(methodNotAllowedFilterHolder, "/logout", EnumSet.of(DispatcherType.REQUEST));
+		
+		ContextHandlerCollection contexts = new ContextHandlerCollection();
+		contexts.setHandlers(new Handler[] { apiContext, rootContext });
+		server.setHandler(contexts);
+
+		server.start();
+		server.join();
+	}
+}
