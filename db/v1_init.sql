4acef5dea6908c0796759309136149bcf0bf1e37
Humberto <humbertoodantas@gmail.com>
2023-05-07 23:34:24 -0300

    DROP table removed from SQL file

--- db/v1_init.sql
+++ db/v1_init.sql
@@ -1,14 +1,4 @@
-DROP TABLE IF EXISTS `access_confirmations`;
-$
-DROP TABLE IF EXISTS `email_verifications`;
-$
-DROP TABLE IF EXISTS `otps`;
-$
-DROP TABLE IF EXISTS `sessions`;
-$
-DROP TABLE IF EXISTS `users`;
-$
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
 	`user_id` int AUTO_INCREMENT PRIMARY KEY,
 	`first_name` varchar(250),
 	`last_name` varchar(250),
@@ -17,7 +7,7 @@ CREATE TABLE `users` (
 	`updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 $
-CREATE TABLE `sessions` (
+CREATE TABLE IF NOT EXISTS `sessions` (
 	`session_id` int AUTO_INCREMENT PRIMARY KEY,
 	`user_id` int NOT NULL,
 	`session` varchar(250) UNIQUE NOT NULL,
@@ -27,7 +17,7 @@ CREATE TABLE `sessions` (
 	INDEX idx_user_id (user_id)
 );
 $
-CREATE TABLE `otps` (
+CREATE TABLE IF NOT EXISTS `otps` (
 	`otp_id` int AUTO_INCREMENT PRIMARY KEY,
 	`user_id` int NOT NULL,
 	`otp` varchar(250) UNIQUE NOT NULL,
@@ -37,7 +27,7 @@ CREATE TABLE `otps` (
 	INDEX idx_user_id (user_id)
 );
 $
-CREATE TABLE `email_verifications` (
+CREATE TABLE IF NOT EXISTS `email_verifications` (
 	`email_verification_id` int AUTO_INCREMENT PRIMARY KEY,
 	`user_id` int NOT NULL,
 	`is_confirmed` boolean NOT NULL,
@@ -46,7 +36,7 @@ CREATE TABLE `email_verifications` (
 	INDEX idx_user_id (user_id)
 );
 $
-CREATE TABLE `access_confirmations` (
+CREATE TABLE IF NOT EXISTS `access_confirmations` (
 	`access_confirmations_id` int AUTO_INCREMENT PRIMARY KEY,
 	`user_id` int NOT NULL,
 	`token` varchar(250) UNIQUE NOT NULL,
